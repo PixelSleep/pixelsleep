0746006031cd7e60786ef9d71f10dcaa694fa83c
Robin Van den Broeck <robin.broeck@gmail.com>
2016-05-24 22:11:59 +0200

    Added vagrantfile for mysql

--- src/database.js
+++ src/database.js
@@ -1,9 +1,11 @@
 import {createConnection} from 'mysql';
 
-export const connection = createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'secret',
-  database: 'pixelsleep',
-});
+const settings = {
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_DATABASENAME || 'pixelsleep',
+}
+
+export const connection = createConnection(settings);
