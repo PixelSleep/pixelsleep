ea764edf3a85d223ff7511c032dd47884f1943e9
Robin Van den Broeck <robin.broeck@gmail.com>
2016-05-24 22:29:14 +0200

    Made installer

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 import {createConnection} from 'mysql';
 
-const settings = {
+export const settings = {
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USERNAME || 'root',
--- src/install.js
+++ src/install.js
@@ -1,7 +1,28 @@
-import {connection} from './database';
+import {connection, settings} from './database';
 
+// Connect to the database
 connection.connect(err => {
+  // If there is an error, display the message and exit the program
   if(err) {
     console.error('Error connection' + err.stack);
+    process.exit();
   }
+  console.log('Succesfully connected as id %d', connection.threadId);
+});
+
+connection.query(`
+  CREATE TABLE ${settings.database}.\`hours\` (
+    \`id\` INT NOT NULL,
+    \`tijd_gaan_slapen\` DATETIME,
+    \`tijd_opgestaan\` DATETIME,
+    \`gewenste_slaaptijd\` DATETIME,
+    PRIMARY KEY (\`id\`)
+  );
+`, err => {
+  if(err) console.log(`Err: ${err}`);
+});
+
+// End the connection
+connection.end(err => {
+  if(err) console.error('Error ending connection' + err.stack);
 });
