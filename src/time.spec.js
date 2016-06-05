95aab6157610cf3f5980d4c589cee401dca36bf5
Robin Van den Broeck <robin.broeck@gmail.com>
2016-06-05 22:28:28 +0200

    Wrote test for padding

--- src/time.spec.js
+++ src/time.spec.js
@@ -1,9 +1,20 @@
 import { expect } from 'chai';
-import { calculateDifference } from './time';
+import { pad, calculateDifference } from './time';
 
 describe('time', () => {
+  describe('pad', () => {
+    it('should left pad correctly', () => {
+      expect(pad(0)).to.equal('00');
+      expect(pad('0')).to.equal('00');
+      expect(pad(10)).to.equal('10');
+      expect(pad('10')).to.equal('10');
+      expect(pad(100)).to.equal('100');
+      expect(pad('100')).to.equal('100');
+      expect(pad('010')).to.equal('010');
+    });
+  });
   describe('#calculateDifference', () => {
-    it('should return the correct time', () => {
+    it('should calculate the correct time given 2 valid times', () => {
       expect(calculateDifference('00:00', '12:00')).to.equal('12:00');
       expect(calculateDifference('12:00', '12:00')).to.equal('00:00');
       expect(calculateDifference('11:59', '12:01')).to.equal('00:02');
